ae862b58086c376ce905a4608dae73d11aa32b77
jianpeng_Ubuntu <xujianpeng213@outlook.com>
2022-01-15 06:23:55 -0800

    CMakeLists.txt

---
+++ Src/File.cpp
@@ -0,0 +1,7 @@
+#include "File.h"
+#include <boost/filesystem.hpp>
+
+void getFileList(const std::string &path, std::vector<std::string> &fileList)
+{
+
+}
---
+++ Src/File.h
@@ -0,0 +1,9 @@
+#ifndef __FILE_H_
+#define __FILE_H_
+
+#include <vector>
+#include <string>
+
+void getFileList(const std::string &path, std::vector<std::string> &fileList);
+
+#endif
